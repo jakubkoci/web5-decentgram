5beb4854a9f2e057a84e4d26846096e2f2c0f766
Jakub Koci <jakub.koci@gmail.com>
2024-01-06 16:18:20 +0100

    Patch Signing key not found for author

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { truncate, truncateDid } from '@/utils'
 
 // https://developer.tbd.website/docs/web5/learn/protocols/
 // protocol validator UI https://radiant-semifreddo-af73bb.netlify.app/
-import protocolDefinition from '../protocol.json' with { type: "json" };
+import protocolDefinition from '../protocol.json' with { type: 'json' }
 
 interface RecordWithContent {
   record: Record
@@ -18,32 +18,33 @@ const configureProtocol = async (web5: Web5, did: string) => {
     message: {
       filter: {
         protocol: protocolDefinition.protocol,
-      }
-    }
-  });
+      },
+    },
+  })
 
   if (status.code !== 200) {
-    console.error('Error querying protocols', status);
-    return;
+    console.error('Error querying protocols', status)
+    return
   }
 
   if (protocols.length > 0) {
-    console.log('Protocol already deployed');
-    return;
+    console.log('Protocol already deployed')
+    return
   }
 
   // configure protocol on local DWN
-  const { status: configureStatus, protocol } = await web5.dwn.protocols.configure({
-    message: {
-      definition: protocolDefinition,
-    }
-  });
+  const { status: configureStatus, protocol } =
+    await web5.dwn.protocols.configure({
+      message: {
+        definition: protocolDefinition,
+      },
+    })
 
-  console.log('Protocol configured status', configureStatus);
+  console.log('Protocol configured status', configureStatus)
 
   if (!protocol) throw new Error('Protocol client has not been configured.')
 
-  await protocol.send(did);
+  await protocol.send(did)
 }
 
 export default function Home() {
@@ -57,7 +58,9 @@ export default function Home() {
   useEffect(() => {
     const initWeb5 = async () => {
       console.log('connect web5 client')
-      const { web5, did } = await Web5.connect()
+      const { web5, did } = await Web5.connect({
+        techPreview: { dwnEndpoints: ['http://localhost:4000'] },
+      })
       setWeb5(web5)
       setMyDid(did)
 
@@ -75,7 +78,7 @@ export default function Home() {
       .then(({ web5, did }) => loadAll(web5, did, ''))
   }, [])
 
-  const upload = async (web5: Web5 | null,  did: string, content: File) => {
+  const upload = async (web5: Web5 | null, did: string, content: File) => {
     console.log('upload')
     if (!web5) throw new Error('Web5 client has not been initialized.')
     const { record } = await web5.dwn.records.create({
@@ -102,33 +105,39 @@ export default function Home() {
     console.log('load records')
     if (!web5) throw new Error('Web5 client has not been initialized.')
 
-    const myLocalRecords = await getMyRecords(web5, "")
-    console.log("My local records", myLocalRecords)
+    const myLocalRecords = await getMyRecords(web5, '')
+    console.log('My local records', myLocalRecords)
 
     const myRemoteRecords: ConcatArray<Record> = await getMyRecords(web5, did)
-    console.log("My remote records", myRemoteRecords)
+    console.log('My remote records', myRemoteRecords)
 
-    const sharedRecords: ConcatArray<Record> = await getSharedRecords(web5, theirDid)
-    console.log("Shared records", sharedRecords)
+    const sharedRecords: ConcatArray<Record> = await getSharedRecords(
+      web5,
+      theirDid,
+    )
+    console.log('Shared records', sharedRecords)
 
     const records = new Map()
     await Promise.all(
-      myLocalRecords.concat(myRemoteRecords).concat(sharedRecords).map(async (record: Record) => {
-        const content = await record.data.blob()
-        logRecord(record, await content.text())
-        records.set(record.id, { record, content });
-      }),
+      myLocalRecords
+        .concat(myRemoteRecords)
+        .concat(sharedRecords)
+        .map(async (record: Record) => {
+          const content = await record.data.blob()
+          logRecord(record, await content.text())
+          records.set(record.id, { record, content })
+        }),
     )
     setRecords(Array.from(records.values()))
   }
 
-  const getMyRecords = async (web5: Web5, did: string | "") => {
+  const getMyRecords = async (web5: Web5, did: string | '') => {
     const queryResult = await web5.dwn.records.query({
       from: did,
       message: {
         filter: {
           protocol: protocolDefinition.protocol,
-          dataFormat: protocolDefinition.types.post.dataFormats[0]
+          dataFormat: protocolDefinition.types.post.dataFormats[0],
         },
       },
     })
@@ -150,11 +159,11 @@ export default function Home() {
           filter: {
             protocol: protocolDefinition.protocol,
             // schema: protocolDefinition.types.post.schema,
-            dataFormat: protocolDefinition.types.post.dataFormats[0]
+            dataFormat: protocolDefinition.types.post.dataFormats[0],
           },
         },
       })
-      console.log("their records" + did, queryResult.records)
+      console.log('their records' + did, queryResult.records)
       records = records.concat(queryResult.records || [])
     }
     return records
@@ -177,7 +186,7 @@ export default function Home() {
     if (!recordResult.record) throw new Error('Record ID not found.')
 
     const recipientDid = prompt('Enter recipient DID')
-    if (!recipientDid || recipientDid.length !== 1215) throw new Error('Record ID must not be empty.')
+    if (!recipientDid) throw new Error('Record ID must not be empty.')
 
     const recordsWiteResponse = await web5.dwn.records.create({
       data: await recordResult.record.data.blob(),
@@ -195,7 +204,7 @@ export default function Home() {
       throw new Error('Record has not been created from other record')
     }
     console.log('record created', record.id)
-    const { status } = await record.send(recipientDid)
+    const { status } = await record.send(myDid)
     console.log('uploaded', status)
   }
 
@@ -284,7 +293,6 @@ export default function Home() {
             <tr>
               <th>ID</th>
               <th>Content</th>
-              <th>Target</th>
               <th>Author</th>
               <th>Recipient</th>
               <th>Actions</th>
@@ -303,7 +311,6 @@ export default function Home() {
                       height={100}
                     />
                   </td>
-                  <td>{truncateDid(record.target)}</td>
                   <td>{truncateDid(record.author)}</td>
                   <td>
                     {record.recipient ? truncateDid(record.recipient) : 'no'}
